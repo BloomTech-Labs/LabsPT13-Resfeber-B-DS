e0a3b03e2a9ffe89698b11691918ff3e71b42bea
jpeone <jesse.gene.peone@gmail.com>
2020-12-06 20:55:14 -0800

    input object created for gas predictions, edits to support new elastic beanstalk process

--- project/app/api/predict.py
+++ project/app/api/predict.py
@@ -4,6 +4,7 @@ import random
 from fastapi import APIRouter
 import pandas as pd
 from pydantic import BaseModel, Field, validator
+from typing import Optional
 
 log = logging.getLogger(__name__)
 router = APIRouter()
@@ -26,6 +27,18 @@ class Item(BaseModel):
         assert value > 0, f'x1 == {value}, must be > 0'
         return value
 
+class GasItem(BaseModel):
+    '''
+    Use this data model to parse the request body JSON for gas predictions.
+    '''
+
+    coords: str = Field(..., example='long,lat;long,lat;long,lat')
+    month: int = Field(..., example = 7)
+    day: int = Field(..., example = 13)
+    year: int = Field(..., example = 2021)
+    mpg: Optional[float] = None
+
+    # TODO: Put in validators here. We are doing this live for first attempt
 
 @router.post('/predict')
 async def predict(item: Item):
@@ -53,3 +66,32 @@ async def predict(item: Item):
         'prediction': y_pred,
         'probability': y_pred_proba
     }
+
+@router.post('/predict/gas')
+async def predict_gas(item: GasItem):
+    '''
+    Predicts the total cost of gas for a road trip between coordinates. 
+
+    ### Request Body
+    - 'coords': a string of semicolon separated coordinate pairs formated as 
+    'long,lat;long,lat;long,lat'. Each coordinate pair represents a stop on the 
+    user's road trip.
+    - 'month': an integer containing the month of the road trip
+    - 'day': an integer containing the day of the road trip
+    - 'year': an integer containing the year of the road trip
+    - 'mpg': an optional float specifying the miles per gallon of the vehicle
+    being used on the road trip. If no value is passed, 27 mpg will be used as
+    a default value
+
+    ### Response
+    - 'prediction': a float with the total cost of of gas for the entire length
+    of the trip.
+    '''
+
+@router.get('/test')
+async def test():
+    '''
+    A very simple test get request
+    '''
+
+    return 'Test worked'
